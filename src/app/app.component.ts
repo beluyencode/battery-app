f8e9c61cffc4c5cd99adc92cd739d4f2a0742d37
beluyencode <longlc956@gmail.com>
2023-03-20 22:44:30 +0700

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { timer } from 'rxjs';
+import { faBoltLightning } from '@fortawesome/free-solid-svg-icons';
+import { faCloud } from '@fortawesome/free-solid-svg-icons';
+import { faBuilding } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'battery-app';
+export class AppComponent implements OnInit {
+  percent = 0;
+  icon = faBoltLightning;
+  cloud = faCloud;
+  build = faBuilding
+
+  ngOnInit(): void {
+    const count = timer(1000, 500).subscribe(() => {
+      if (this.percent < 100) {
+        this.percent += 1;
+      } else {
+        count.unsubscribe();
+      }
+    })
+  }
+
 }
